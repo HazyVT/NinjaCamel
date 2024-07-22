a87d1a0060dac66a4ccf3521c72c11f104e0e6a0
Mohammed Salim <renablehots@gmail.com>
2024-07-22 13:51:33 -0400

    Added new xp bar

--- Assets/Scripts/UI/ExperienceBarBehaviour.cs
+++ Assets/Scripts/UI/ExperienceBarBehaviour.cs
@@ -1,21 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ExperienceBarBehaviour : MonoBehaviour
 {
-    public GameObject bar;
+    //public GameObject bar;
+    private float fillAmount;
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.localScale = new(0, 0.3f);
+        //transform.localScale = new(0, 0.3f);
+        fillAmount = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float size = ExperienceManager.experience / ExperienceManager.level;
-        bar.transform.localScale = new(size / 100, bar.transform.localScale.y, bar.transform.localScale.z);
+        //float size = ExperienceManager.experience / ExperienceManager.level;
+        //bar.transform.localScale = new(size / 100, bar.transform.localScale.y, bar.transform.localScale.z);
+        fillAmount = ExperienceManager.experience / ExperienceManager.level / 100;
+        //fillAmount += Time.deltaTime;
+        GetComponent<Image>().fillAmount = fillAmount;
     }
 }
