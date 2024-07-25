b741c84c1be8b14ae2dc6dc8193122c9bd35d855
Mohammed Salim <renablehots@gmail.com>
2024-07-25 11:08:19 -0400

    Tutorial added

--- Assets/TutorialScript.cs
+++ Assets/TutorialScript.cs
@@ -14,7 +14,6 @@ public class TutorialScript : MonoBehaviour
     void Update()
     {
         Touch touch = Input.GetTouch(0);
-
         if (touch.phase == TouchPhase.Began) {
             gameObject.SetActive(false);
         }
